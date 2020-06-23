d9cad1d9e20a0cd5d8c170f25be73ed565d6754c
Koba <koba_test@mail.ru>
2020-06-23 15:26:14 +0700

    ex3.py - final version i think

--- ex3.py
+++ ex3.py
@@ -2,7 +2,8 @@ import xlrd,sys
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-print(sys.argv)
+
+#print(sys.argv)
 if len(sys.argv) < 2:
     print('А какие листы-то сравнивать?')
     while(True):
@@ -16,12 +17,24 @@ else:
         list_num = int(sys.argv[1])
     except ValueError:
         list_num = 0
-print(list_num)
-'''
-Tk().withdraw()
-filename = askopenfilename(initialdir = "/", filetypes = (("Excel files","*.xls"),("all files","*.*")))
-print(filename)
+if list_num<=0:
+    list_num = 0
+else:
+    list_num = list_num - 1
+print("Лист: ", list_num)
 
+Tk().withdraw()
+filename1 = askopenfilename(initialdir = "c:/Apache2.2/htdocs/xls", title = "Первый файл", filetypes = (("Excel files","*.xls"),("all files","*.*")))
+
+Tk().withdraw()
+filename2 = askopenfilename(initialdir = "c:/Apache2.2/htdocs/xls", title = "Второй файл", filetypes = (("Excel files","*.xls"),("all files","*.*")))
+
+if filename1 == "": filename1 = "u1.xls"
+if filename2 == "": filename2 = "u2.xls"
+print("Первый файл: " + filename1)
+print("Второй файл: " + filename2)
+
+'''
         if sheet1.cell_value(row, col) == '':
             print('BLANK')
         else:
@@ -31,9 +44,9 @@ out_f = open("cmp.html",'w')
 out_f1 = open("cmp1.txt",'w')
 out_f2 = open("cmp2.txt",'w')
 
-rb1 = xlrd.open_workbook('u1.xls',formatting_info=False, on_demand=True)
+rb1 = xlrd.open_workbook(filename1,formatting_info=False, on_demand=True)
 sheet1 = rb1.sheet_by_index(list_num)
-rb2 = xlrd.open_workbook('u2.xls',formatting_info=False, on_demand=True)
+rb2 = xlrd.open_workbook(filename2,formatting_info=False, on_demand=True)
 sheet2 = rb2.sheet_by_index(list_num)
 rb1.release_resources
 rb2.release_resources
@@ -46,7 +59,9 @@ for row in range(sheet1.nrows):
             #print(xlrd.cellname(row, col), '-------------NOT MATCH---------------')
             #t = (row, col)
             cmp.append((row, col))
+            #print(row, col, str(sheet1.cell_value(row, col)))
     #row = sheet1.row_values(rownum)
+
 out_f.write('<html><body>Список различий:<br>')
 for c in cmp:
     row, col = c
